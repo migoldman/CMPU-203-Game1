bec9bbc85af6fd0e9dd95fb76401a032942a7308
Michael Goldman <migoldman@vassar.edu>
2014-10-07 14:06:22 -0400

    Trying to reboot netbeans
    So far so good…

--- src/cmpu/pkg203/game1/TetrisWorld.java
+++ src/cmpu/pkg203/game1/TetrisWorld.java
@@ -26,11 +26,14 @@ public class TetrisWorld extends World {
     static final int side = 80;
     static final int rows = wWidth/side;
     static final int columns = wHeight/side;
+    static int[][] worldArray = new int[columns][rows];
+    int counter;
     int dx;
     int dy;
     public WorldImage world;
     Shapes user;
     LinkedList placedShapes;
+    
     public TetrisWorld(Shapes user, LinkedList placedShapes, int x, int y, int dx, int dy) {
         this.user = user;
         this.placedShapes = placedShapes;
@@ -40,12 +43,33 @@ public class TetrisWorld extends World {
         this.dy = dy;
     }
     
-    public void keyPressed(KeyEvent e){
+    public TetrisWorld tick() {
+    	int newX = x + dx;
+    	int newY = y + dy;
+    	if(newX <= 0) {
+    		return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, 0, -dy);    	
+    		}
+    	if(newX >= columns) {
+    		return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, 0, -dy);
+    	}
+    	if(newY = 0) {
+    		return new TetrisWorld(user.isPlaced(), )
+    	}
+    }
+    
+    public TetrisWorld keyPressed(KeyEvent e){
     	int keyCode = e.getKeyCode();
     	switch(keyCode) {
+    	case KeyEvent.VK_LEFT:
+    		return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, dx, -dy);   
+    	case KeyEvent.VK_RIGHT:
+    		return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, -dx, -dy);
+    	case KeyEvent.VK_DOWN:
+    		return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, 0, -dy*2);
     	case KeyEvent.VK_UP:
-    		//check if it can move
-    		
+    		return new TetrisWorld(this.user.rotate(),this.placedShapes, this.x, this.y, 0, -dy);
+		default:
+			return new TetrisWorld(this.user, this.placedShapes, this.x, this.y, 0, -dy);
     	}
     }
  
