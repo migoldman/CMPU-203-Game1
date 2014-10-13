a7c8da9e6ade05f82df6449bd629ef2f99890280
Michael Goldman <migoldman@vassar.edu>
2014-10-13 15:54:40 -0400

    Midwork commit
    because of harry

--- src/cmpu/pkg203/game1/Shapes.java
+++ src/cmpu/pkg203/game1/Shapes.java
@@ -10,7 +10,6 @@ package cmpu.pkg203.game1;
  *
  * @author michaelgoldman
  */
-import java.util.Random;
 enum Rotation {
     UP, LEFT, RIGHT, DOWN;
 }
@@ -22,237 +21,9 @@ public class Shapes {
     int x, y;
     ShapeType block;
     Rotation orientation;
-    Random random;
     int squareMatrix[][];
     //[type][rotation][x of grid][y of grid]
-    int SHAPES[][][][] = {
-    //This defines all the shapes, a matrix where 1 is
-        //where blocks will be and 0 will be empty
-           
-        //TYPE
-            //ROTATION
-                //X AND Y
-        
-        //Square shaped
-        {
-            {
-                //UP
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,1,0}
-            },
-            {
-                //LEFT
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,1,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,1,0}
-            },
-            {
-                //RIGHT
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,1,0}
-            },
-        },
-        
-        //S shape
-        {
-            {
-                //UP
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {1,1,0,0}
-            }, 
-            {
-                //LEFT
-                {0,0,0,0},
-                {1,0,0,0},
-                {1,1,0,0},
-                {0,1,0,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,0},
-                {1,1,0,0}
-            }, 
-            {
-                //RIGHT
-                {0,0,0,0},
-                {1,0,0,0},
-                {1,1,0,0},
-                {0,1,0,0}
-            },
-        },
-        
-        //Line shape
-        {
-            {
-                //UP
-                {1,0,0,0},
-                {1,0,0,0},
-                {1,0,0,0},
-                {1,0,0,0}
-            },
-            {
-                //LEFT
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,0,0},
-                {1,1,1,1}
-            },
-            {
-                //DOWN
-                {1,0,0,0},
-                {1,0,0,0},
-                {1,0,0,0},
-                {1,0,0,0}
-            },
-            {
-                //RIGHT
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,0,0},
-                {1,1,1,1}
-            },
-        },
-        //T shape
-        {
-            {
-                //UP
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,1,0},
-                {0,1,1,1},
-            },
-            {
-                //LEFT
-                {0,0,0,0},
-                {0,0,1,0},
-                {0,1,1,0},
-                {0,0,1,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,1,1,1},
-                {0,0,1,0}
-            },
-            {
-                //RIGHT
-                {0,0,0,0},
-                {0,0,1,0},
-                {0,0,1,1},
-                {0,0,1,0}
-            }
-        },
-        //Z shape
-        {
-            {
-                //UP
-                {0,0,0,0}, 
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,0,1,1}
-            },
-            {
-                //LEFT
-                {0,0,0,0}, 
-                {0,0,0,1},
-                {0,0,1,1},
-                {0,0,1,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0}, 
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,0,1,1}
-            },
-            {
-                //RIGHT
-                {0,0,0,0}, 
-                {0,0,0,1},
-                {0,0,1,1},
-                {0,0,1,0}
-            },
-        },
-        //L shaped
-        {
-            {
-                //UP
-                {0,0,0,0},
-                {0,1,0,0},
-                {0,1,0,0},
-                {0,1,1,0},
-            },
-            {
-                //LEFT
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,1,0},
-                {1,1,1,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,0,1,0},
-                {0,0,1,0}
-            },
-            {
-                //RIGHT
-                {0,0,0,0},
-                {0,0,0,0},
-                {1,1,1,0},
-                {1,0,0,0}
-            },
-        },
-        //reverse L shaped
-        {
-            {
-                //UP
-                {0,0,0,0},
-                {0,0,1,0},
-                {0,0,1,0},
-                {0,1,1,0}
-            },
-            {
-                //LEFT
-                {0,0,0,0},
-                {0,0,0,0},
-                {1,1,1,0},
-                {0,0,1,0}
-            },
-            {
-                //DOWN
-                {0,0,0,0},
-                {0,1,1,0},
-                {0,1,0,0},
-                {0,1,0,0}
-            },
-            {
-                //RIGHT
-                {0,0,0,0},
-                {0,0,0,0},
-                {1,0,0,0},
-                {1,1,1,0}
-            }
-        },
-    };
+    
     
     //What is in a shape
         //Has the type of shape it is
@@ -305,17 +76,6 @@ public class Shapes {
         }
     }
     
-    public int[][] getMatrix() {
-        Shapes temp = new Shapes(this.block,this.orientation,this.x,this.y);
-        return SHAPES[temp.getType()][temp.getOrientation()];
-    }
-    
-    //will check if the location in the matrix should be a block
-    public boolean isBlockHuh(int xM, int yM) {
-        Shapes temp = new Shapes(this.block, this.orientation, this.x, this.y);
-        return temp.getMatrix()[xM][yM] == 1;
-    }
-
     public Shapes setPos(int x, int y) {
         this.x = x;
         this.y = y;
@@ -339,30 +99,5 @@ public class Shapes {
                 temp = new Shapes(this.block,Rotation.UP,this.x,this.y);
         }
         return temp;
-    }  
-    
-    //makes a random int 1-7 for block types
-    public int randomInt() {
-        return random.nextInt((0 - 6) + 1) + 0;
-    }
-          
-    public Shapes makeBlock(int number){
-        switch(number) {
-        	case 1:
-                    return new Shapes(ShapeType.SQUARE, Rotation.UP, 5, 0);
-        	case 2:
-                    return new Shapes(ShapeType.S, Rotation.UP, 5, 0);
-        	case 3:
-                    return new Shapes(ShapeType.LINE, Rotation.UP, 5, 0);
-        	case 4:
-                    return new Shapes(ShapeType.T, Rotation.UP, 5, 0);
-        	case 5:
-                    return new Shapes(ShapeType.Z, Rotation.UP, 5, 0);
-        	case 6:
-                    return new Shapes(ShapeType.L, Rotation.UP, 5, 0);
-    		default:
-                    return new Shapes(ShapeType.rL, Rotation.UP, 5, 0);
-        }
     }
 }
-    
--- src/cmpu/pkg203/game1/TetrisWorld.java
+++ src/cmpu/pkg203/game1/TetrisWorld.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cmpu.pkg203.game1;
 
 /**
  *
  * @author michaelgoldman
  */
+import java.util.Random;
 import java.util.LinkedList;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
@@ -17,93 +17,418 @@ import java.awt.*;
 
 public class TetrisWorld {
     
-
     static final int rows = 20;
     static final int columns = 10;
     static int[][] worldArray;
+    Random random;
     int dx;
     int dy;
     Shapes user;
     LinkedList<Shapes> placedShapes;
-    
+    TetrisWorld world;
+
+    static final int SHAPES[][][][] = {
+        //This defines all the shapes, a matrix where 1 is
+        //where blocks will be and 0 will be empty
+
+        //TYPE
+        //ROTATION
+        //X AND Y
+
+        //Square shaped
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 1, 0}
+            },
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 1, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 1, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 1, 0}
+            },},
+        //S shape
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {1, 1, 0, 0}
+            },
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 1, 0, 0},
+                {0, 1, 0, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {1, 1, 0, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 1, 0, 0},
+                {0, 1, 0, 0}
+            },},
+        //Line shape
+        {
+            {
+                //UP
+                {1, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 0, 0, 0}
+            },
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 1}
+            },
+            {
+                //DOWN
+                {1, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 0, 0, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 1}
+            },},
+        //T shape
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 1, 1, 1},},
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 1, 1, 0},
+                {0, 0, 1, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 1},
+                {0, 0, 1, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 1, 1},
+                {0, 0, 1, 0}
+            }
+        },
+        //Z shape
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 1, 1}
+            },
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 0, 1, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 1, 1}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 0, 1, 0}
+            },},
+        //L shaped
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 1, 0},},
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {1, 1, 1, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 1, 0},
+                {0, 0, 1, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 0},
+                {1, 0, 0, 0}
+            },},
+        //reverse L shaped
+        {
+            {
+                //UP
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 1, 0},
+                {0, 1, 1, 0}
+            },
+            {
+                //LEFT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 0},
+                {0, 0, 1, 0}
+            },
+            {
+                //DOWN
+                {0, 0, 0, 0},
+                {0, 1, 1, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0}
+            },
+            {
+                //RIGHT
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 0, 0, 0},
+                {1, 1, 1, 0}
+            }
+        },};
+
     //Make the world a dense matrix of falses, then add in graphics
     public TetrisWorld(Shapes user, LinkedList<Shapes> placedShapes) {
         this.user = user;
         this.placedShapes = placedShapes;
     }
-    
-    //Makes an empty world (a world of 0s)
-    public TetrisWorld newWorld() {
-        for(int x = 0; x < columns; x++) {
-            for(int y = 0; y < rows; y++) {
-                worldArray[x][y] = 0;
-            }
-        }
-        return new TetrisWorld(user.makeBlock(user.randomInt()), placedShapes);
-    }
-    
 
-    
+    //Makes an empty world
+    public TetrisWorld newWorld() {
+        return new TetrisWorld(makeBlock(randomInt()), new LinkedList());
+    }
+
+    //makes a random int 1-7 for block types
+    public int randomInt() {
+        return random.nextInt((7 - 1) + 1) + 1;
+    }
+
+    public Shapes makeBlock(int number) {
+        switch (number) {
+            case 1:
+                return new Shapes(ShapeType.SQUARE, Rotation.UP, 5, 0);
+            case 2:
+                return new Shapes(ShapeType.S, Rotation.UP, 5, 0);
+            case 3:
+                return new Shapes(ShapeType.LINE, Rotation.UP, 5, 0);
+            case 4:
+                return new Shapes(ShapeType.T, Rotation.UP, 5, 0);
+            case 5:
+                return new Shapes(ShapeType.Z, Rotation.UP, 5, 0);
+            case 6:
+                return new Shapes(ShapeType.L, Rotation.UP, 5, 0);
+            default:
+                return new Shapes(ShapeType.rL, Rotation.UP, 5, 0);
+        }
+    }
+
+    public int[][] getMatrix() {
+        Shapes temp = new Shapes(user.block, user.orientation, user.x, user.y);
+        return SHAPES[temp.getType()][temp.getOrientation()];
+    }
+
+    //will check if the location in the matrix should be a block
+    public boolean isBlockHuh(int xM, int yM) {
+        //this is bad coding. Will hopefully fix
+        world = new TetrisWorld(this.user, this.placedShapes);
+        return world.getMatrix()[xM][yM] == 1;
+    }
+
     //checks if the block can keep moving down
-    public boolean isPlacedHuh() {
+    public boolean aboveBlockHuh() {
         int xPos = user.x;
-        int yPos = user.y;    
-        for(int i = 0; i< placedShapes.size(); i++) {
-            if(yPos-1 == placedShapes.get(i).y && 
-                xPos == placedShapes.get(i).x) {
+        int yPos = user.y;
+        for (Shapes placedShape : placedShapes) {
+            if (yPos + 1 == placedShape.y && xPos == placedShape.x) {
                 return true;
             }
         }
-        if(yPos == 20) {
+        if (yPos == 20) {
             return true;
-        }
-        else {
-            return false; 
+        } else {
+            return false;
         }
     }
     
-    //BAD
-    /*public TetrisWorld tick() {
-    	int newX = user.x + dx;
-    	int newY = user.y + dy;
-    	if(newX <= 0) {
-            return new TetrisWorld(this.user, this.placedShapes);    	
-            }
-    	if(newX >= columns) {
-            return new TetrisWorld(this.user, this.placedShapes);
-    	}
-    	if(isPlacedHuh()) {
-            placedShapes.add(user);
-            return new TetrisWorld(user.makeBlock(user.randomInt()), placedShapes);
-    	}
-    }*/
+    UP, LEFT, RIGHT, DOWN;
+    SQUARE, S, LINE, T, Z, L, rL;
     
+    public int getWidth(Shapes block, int column) {
+        int type = block.getType();
+        int orientation = block.getOrientation();
+        switch(type) {
+            case 0: // square
+                return 2;
+            case 1: //s
+                switch(orientation) {
+                    case 0:
+                        return 3;
+                    case 1:
+                        return 2; 
+                    case 2:
+                        return 3;
+                    case 3:
+                        return 2;
+            }
+            case 2: //line
+                switch(orientation) {
+                    case 0:
+                        return 1;
+                    case 1:
+                        return 4;
+                    case 2:
+                        return 1;
+                    case 3:
+                        return 4;
+            }
+            case 3: //t
+                switch(orientation) {
+                    case 0:
+                        return 3;
+                    case 1:
+                        return 2;
+                    case 2:
+                        return 3;
+                    case 3:
+                        return 2;
+            }
+            case 4: //z
+                switch(orientation) {
+                    case 0:
+                        return 3;
+                    case 1:
+                        return 2;
+                    case 2:
+                        return 3;
+                    case 3:
+                        return 2;
+            }
+            case 5: //l
+                switch(orientation) {
+                    case 0:
+                        return 
+                    case 1:
+                    case 2:
+                    case 3:
+            }
+            case 6: //rl
+                switch(orientation) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+            }
+            default:
+                throw new NullPointerException();
+        }
+    }
+    
+    public boolean isValidHuh(Shapes block) {
+        int xPos = block.x;
+        int yPos = block.y;
+        for (Shapes placedShape : placedShapes) {
+            if (yPos == placedShape.y && xPos == placedShape.x) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
     //Think it is good
-    public TetrisWorld keyPressed(KeyEvent e){
-    	int keyCode = e.getKeyCode();
+    public TetrisWorld keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        TetrisWorld temp;
         int x = user.x;
         int y = user.y;
-        int newX = x + dx;
-        int newY = y + 1;
-    	switch(keyCode) {
+        int newY = y + 1; 
+        //Block is always moving down, while can move left or right with x
+        switch (keyCode) {
             case KeyEvent.VK_LEFT:
-                if(newX > 0) {
-                    return new TetrisWorld(this.user.setPos(x-1, newY), this.placedShapes);   
+                if (x-1 >= 0) {
+                    temp = new TetrisWorld(this.user.setPos(x - 1, newY), this.placedShapes);
+                } else {
+                    temp = new TetrisWorld(this.user.setPos(x, newY), this.placedShapes);
                 }
-                else {
-                    return new TetrisWorld(this.user.setPos(x, newY), this.placedShapes);
-                }
+                break;
             case KeyEvent.VK_RIGHT:
-                if(newX < columns) {
-                    return new TetrisWorld(this.user.setPos(x+1, newY), this.placedShapes);
+                if (x+1 <= columns) {
+                    temp = new TetrisWorld(this.user.setPos(x + 1, newY), this.placedShapes);
+                } else {
+                    temp = new TetrisWorld(this.user.setPos(x, newY), this.placedShapes);
                 }
-                else {
-                    return new TetrisWorld(this.user.setPos(x, newY), this.placedShapes);
-                }
+                break;
             case KeyEvent.VK_UP:
-    		return new TetrisWorld(this.user.rotate(),this.placedShapes);
+                if(isValidHuh(user.rotate())) {
+                    temp = new TetrisWorld(this.user.rotate(), this.placedShapes);
+                    break;
+                }
             default:
-                return new TetrisWorld(this.user.setPos(x, y+1), this.placedShapes);
-    	}
+                temp = new TetrisWorld(this.user.setPos(user.x, newY), placedShapes);
+        }
+        return temp;
+    }
+
+    public TetrisWorld tick() {
+        
     }
 }
